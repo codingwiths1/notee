f522ddbe1a558eccbe817e6fead136b8f5a5ac6b
codingwiths1 <codingwiths01@gmail.com>
2025-07-16 19:31:07 +0100

    all_done

--- lib/core/extention/extention.dart
+++ lib/core/extention/extention.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-extension NumExtention on num {
+extension SizedboxExtention on num {
   SizedBox get toVert => SizedBox(height: h);
   SizedBox get toHort => SizedBox(width: w);
 }
