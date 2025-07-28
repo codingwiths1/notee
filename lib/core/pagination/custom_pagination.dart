c16ae5565a5cb64b89355f46f2af9789f638b2ef
codingwiths1 <codingwiths01@gmail.com>
2025-07-28 13:59:13 +0100

    all_done

--- lib/core/pagination/custom_pagination.dart
+++ lib/core/pagination/custom_pagination.dart
@@ -1,38 +1,3 @@
-// import 'dart:developer';
-// import 'dart:ffi';
-
-// import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
-// class CustomPagination<T> {
-//   final int limit;
-//   final PagingController _pagingController = PagingController(firstPageKey: 0);
-//   final Future<List<T>> Function(int offset, int limit) fetchdata;
-
-//   CustomPagination({required this.fetchdata, this.limit = 10}) {
-//     _pagingController.addPageRequestListener((offset) {
-//       _loadmore(offset);
-//     });
-//   }
-//   Future<void> _loadmore(int offset) async {
-//     try {
-//       final newItems = await fetchdata(offset, limit);
-
-//       final isLastPage = newItems.length < limit;
-//       if (isLastPage) {
-//         _pagingController.appendLastPage(newItems);
-//       } else {
-//         final nextPageKey = offset + newItems.length;
-//         _pagingController.appendPage(newItems, nextPageKey);
-//       }
-//     } catch (e) {
-//       _pagingController.error = e;
-//       log("THIS IS ERROR ${_pagingController.error = e}");
-//     }
-//   }
-
-//   void refresh() => _pagingController.refresh();
-//   void dispose() => _pagingController.dispose();
-// }
 import 'dart:developer';
 
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -59,6 +24,7 @@ class CustomPagination<T> {
 
       if (isLastPage) {
         pagingController.appendLastPage(newItems);
+        log("THIS IS LAST ITEM $newItems");
         log("THIS IS LAST OFFSET $isLastPage");
       } else {
         final nextOffset = offset + newItems.length;
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,232 +1,9 @@
-// import 'package:auto_route/auto_route.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:iconsax/iconsax.dart';
-// import 'package:notee/core/component/note_card.dart';
-// import 'package:notee/core/extention/extention.dart';
-// import 'package:notee/core/router/app_router.dart';
-// import 'package:notee/core/theme/theme.dart';
-// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-// import 'package:notee/features/note/view_model/note_bloc/note_cubit.dart';
-// import 'package:notee/features/note/view_model/note_bloc/note_state.dart';
-
-// @RoutePage()
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   @override
-//   void initState() {
-//     context.read<NoteCubit>().updateloaders(loadingnotes: true);
-//     context.read<NoteCubit>().fetchnotes();
-//     super.initState();
-//   }
-
-//   Future<void> refresh() {
-//     context.read<NoteCubit>().updateloaders(loadingnotes: true);
-//     return context.read<NoteCubit>().fetchnotes(isRefresh: true);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<NoteCubit, NoteState>(
-//       builder: (context, state) {
-//         return Scaffold(
-//           floatingActionButtonLocation:
-//               FloatingActionButtonLocation.centerDocked,
-//           appBar: CupertinoNavigationBar(
-//             transitionBetweenRoutes: true,
-//             border: Border.all(color: AppColor.transparant),
-//             leading: Text(
-//               "Keep Note",
-//               style: Theme.of(
-//                 context,
-//               ).textTheme.headlineLarge!.copyWith(fontWeight: FontWeight.bold),
-//             ),
-//           ),
-//           body: RefreshIndicator(
-//             onRefresh: () => refresh(),
-
-//             child: SingleChildScrollView(
-//               physics: const AlwaysScrollableScrollPhysics(),
-//               child: state.loadingnotes
-//                   ? Padding(
-//                       padding: const EdgeInsets.only(top: 30),
-//                       child: GridView.builder(
-//                         shrinkWrap: true,
-//                         physics: const NeverScrollableScrollPhysics(),
-//                         itemCount: 4,
-//                         gridDelegate:
-//                             const SliverGridDelegateWithFixedCrossAxisCount(
-//                               crossAxisCount: 2,
-//                               mainAxisSpacing: 10,
-//                               crossAxisSpacing: 10,
-//                             ),
-//                         itemBuilder: (context, index) => Container(
-//                           height: 70,
-//                           width: double.maxFinite,
-//                           decoration: BoxDecoration(
-//                             borderRadius: BorderRadius.circular(10),
-//                             color: AppColor.secondaryColor,
-//                           ),
-//                           child: Center(
-//                             child: CircularProgressIndicator(
-//                               color: AppColor.blue,
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     )
-//                   : state.notelist.isEmpty
-//                   ? SizedBox(
-//                       height: MediaQuery.of(context).size.height,
-//                       child: Center(
-//                         child: Image.asset(
-//                           "assets/images/empty-wishlist-illustration-download-in-svg-png-gif-file-formats--online-shop-store-state-pack-sign-symbols-illustrations-11838276 (1) (1) (1) (1).png",
-//                           fit: BoxFit.contain,
-//                           color: AppColor.black,
-//                           height: 250,
-//                           width: 250,
-//                         ),
-//                       ),
-//                     )
-//                   : Padding(
-//                       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         children: <Widget>[
-//                           30.toVert,
-//                           Row(
-//                             children: [
-//                               Expanded(
-//                                 child: TextField(
-//                                   decoration: InputDecoration(
-//                                     hintText: "Search note...",
-//                                     prefixIcon: Icon(
-//                                       Iconsax.search_normal,
-//                                       color: AppColor.black,
-//                                     ),
-//                                     suffixIcon: Container(
-//                                       margin: const EdgeInsets.symmetric(
-//                                         horizontal: 10,
-//                                         vertical: 5,
-//                                       ),
-//                                       decoration: BoxDecoration(
-//                                         color: AppColor.white,
-//                                         borderRadius: BorderRadius.circular(10),
-//                                       ),
-//                                       padding: const EdgeInsets.all(5),
-//                                       child: Icon(
-//                                         Icons.view_agenda_rounded,
-//                                         color: AppColor.grey700,
-//                                       ),
-//                                     ),
-//                                     enabledBorder: OutlineInputBorder(
-//                                       borderRadius: BorderRadius.circular(10),
-//                                       borderSide: BorderSide(
-//                                         color: AppColor.transparant,
-//                                       ),
-//                                     ),
-//                                     focusedBorder: OutlineInputBorder(
-//                                       borderRadius: BorderRadius.circular(10),
-//                                       borderSide: BorderSide(
-//                                         color: AppColor.transparant,
-//                                       ),
-//                                     ),
-//                                     filled: true,
-//                                     fillColor: AppColor.secondaryColor,
-//                                   ),
-//                                 ),
-//                               ),
-//                             ],
-//                           ),
-//                           50.toVert,
-//                           MasonryGridView.builder(
-//                             mainAxisSpacing: 10,
-//                             crossAxisSpacing: 10,
-//                             shrinkWrap: true,
-//                             physics: const NeverScrollableScrollPhysics(),
-//                             gridDelegate:
-//                                 const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-//                                   crossAxisCount: 2,
-//                                 ),
-
-//                             itemBuilder: (context, index) {
-//                               final String titleText =
-//                                   state.notelist[index].title;
-//                               final String noteText =
-//                                   state.notelist[index].note;
-//                               final int id = state.notelist[index].id;
-//                               return InkWell(
-//                                 onTap: () => context.router.push(
-//                                   EditNoteRoute(
-//                                     id: id,
-//                                     titleText: titleText,
-//                                     noteText: noteText,
-//                                   ),
-//                                 ),
-//                                 child: NoteCard(
-//                                   titleText: titleText,
-//                                   noteText: noteText,
-//                                 ),
-//                               );
-//                             },
-//                             itemCount: state.notelist.length,
-//                           ),
-//                           10.toVert,
-//                           Center(
-//                             child: !state.loadmore
-//                                 ? ElevatedButton(
-//                                     style: ElevatedButton.styleFrom(
-//                                       shape: const CircleBorder(),
-//                                       backgroundColor: AppColor.blue,
-//                                       foregroundColor: AppColor.white,
-//                                     ),
-//                                     onPressed: () =>
-//                                         context.read<NoteCubit>().loadmore(),
-//                                     child: const Icon(
-//                                       Icons.arrow_drop_down_rounded,
-//                                       size: 40,
-//                                     ),
-//                                   )
-//                                 : SizedBox(
-//                                     height: 30,
-//                                     width: 30,
-//                                     child: CircularProgressIndicator(
-//                                       color: AppColor.blue,
-//                                     ),
-//                                   ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//             ),
-//           ),
-//           floatingActionButton: Padding(
-//             padding: const EdgeInsets.only(bottom: 10.0),
-//             child: FloatingActionButton(
-//               shape: const CircleBorder(),
-//               backgroundColor: AppColor.blue,
-//               onPressed: () => context.router.push(const CreateNoteRoute()),
-//               child: Icon(Icons.add, color: AppColor.white),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:notee/core/router/app_router.dart';
+import 'package:notee/core/theme/theme.dart';
 import 'package:notee/features/note/model/note_model.dart';
 import 'package:notee/features/note/view_model/note_bloc/note_cubit.dart';
 import 'package:notee/features/note/view_model/note_bloc/note_state.dart';
@@ -238,7 +15,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Notes')),
+      appBar: AppBar(
+        backgroundColor: AppColor.backgroundColor,
+        title: const Text('Notes'),
+        elevation: 0,
+        scrolledUnderElevation: 0,
+      ),
       body: BlocBuilder<NoteCubit, NoteState>(
         builder: (context, state) {
           final pagination = state.pagingController;
@@ -248,8 +30,21 @@ class HomePage extends StatelessWidget {
               context.read<NoteCubit>().state.pagingController.refresh();
             },
             child: PagedListView<int, NoteModel>(
+              physics: const BouncingScrollPhysics(),
               pagingController: pagination.pagingController,
               builderDelegate: PagedChildBuilderDelegate<NoteModel>(
+                noItemsFoundIndicatorBuilder: (context) => SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Center(
+                    child: Image.asset(
+                      "assets/images/empty-wishlist-illustration-download-in-svg-png-gif-file-formats--online-shop-store-state-pack-sign-symbols-illustrations-11838276 (1) (1) (1) (1).png",
+                      fit: BoxFit.contain,
+                      color: AppColor.black,
+                      height: 250,
+                      width: 250,
+                    ),
+                  ),
+                ),
                 itemBuilder: (context, note, index) {
                   final String titleText = note.title;
                   final String noteText = note.note;
@@ -263,9 +58,34 @@ class HomePage extends StatelessWidget {
                         noteText: noteText,
                       ),
                     ),
-                    child: ListTile(
-                      title: Text(note.title),
-                      subtitle: Text(note.note),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(10),
+                        ),
+                        tileColor: AppColor.grey700,
+
+                        title: note.title.isNotEmpty
+                            ? Text(
+                                note.title,
+                                style: TextStyle(
+                                  color: AppColor.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            : const SizedBox.shrink(),
+                        subtitle: note.note.isNotEmpty
+                            ? Text(
+                                note.note,
+                                style: TextStyle(color: AppColor.white),
+                              )
+                            : const SizedBox.shrink(),
+                      ),
                     ),
                   );
                 },
@@ -273,6 +93,15 @@ class HomePage extends StatelessWidget {
             ),
           );
         },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: FloatingActionButton(
+          shape: const CircleBorder(),
+          backgroundColor: AppColor.blue,
+          onPressed: () => context.router.push(const CreateNoteRoute()),
+          child: Icon(Icons.add, color: AppColor.white),
+        ),
       ),
     );
   }
